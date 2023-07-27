aaf7042b8252e35f2c5fa124fef3a57b920a6d3c
luisguilheerme <lg.maroni@gmail.com>
2023-07-27 20:14:40 -0300

    Movies by genre, insert review

--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -3,9 +3,12 @@ package com.devsuperior.movieflix.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.MovieCardDTO;
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.repositories.MovieRepository;
@@ -24,6 +27,19 @@ public class MovieService {
 		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Resource not Found"));
 		return new MovieDetailsDTO(entity);
 	}	
+	
+	@Transactional(readOnly = true)
+	public Page<MovieCardDTO> findByGenre(Long id, Pageable pageable) {
+		Page<Movie> result = null;
+		if(id != 0) {
+			result = repository.findByGenreIdOrderByTitle(id, pageable);
+		}
+		else {
+			result = repository.searchAll(pageable);
+		}
+		return result.map(x -> new MovieCardDTO(x));
+	}	
+	
 	
 	
 }
--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -3,9 +3,12 @@ package com.devsuperior.movieflix.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,4 +49,24 @@ public class UserService implements UserDetailsService {
 		
 		return user;
 	}
+	
+	
+	@Transactional(readOnly = true)
+	public UserDTO getMe() {
+		User user = authenticated();
+		return new UserDTO(user);
+	}
+	
+	
+	protected User authenticated() {
+		  try {
+		    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		    Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+		    String username = jwtPrincipal.getClaim("username");
+		    return repository.findByEmail(username);
+		  }
+		  catch (Exception e) {
+		    throw new UsernameNotFoundException("Invalid user");
+		  }
+		}
 }
